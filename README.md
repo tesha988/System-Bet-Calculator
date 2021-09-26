beaeec6b9fe3859cf89d52c3f25d592a40dc8f3a
tesha988 <teshano988@gmail.com>
2021-09-26 16:00:10 +0200

    Done script.js

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # System Bet Calculator
 
-Edited html, almost done with js funcionality, banker bets left to be created.
+Done with js funcionality.
--- script.js
+++ script.js
@@ -1,67 +1,132 @@
 const calculateButton = document.querySelector("#btn");
-const checkboxes = document.querySelectorAll(".choice");
-const odds = document.querySelectorAll(".odds");
 const total = document.querySelector(".total");
 const systemList = document.querySelector("#system");
 const stake = document.querySelector("#stake");
-let systemNum = systemList.options[systemList.selectedIndex].innerText;
+let bankerBets = document.querySelector("#banker-bets");
+let bankerBetsOdds;
+let bankerBetsLabel;
 let systemCombinations = [];
-const array = [];
-const result = [];
+
+const checkedOdds = [];
+const oddsCombinations = [];
 const winning = [];
 
-calculateButton.addEventListener("click", function checkTotal() {
-  total.value = "";
-  let sum = 1;
-  for (i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      let correctOdds = parseFloat(odds[i].value);
-      array.push(correctOdds);
-      // sum = sum * correctOdds;
-      // stakePerCombination = (stake.value / systemList.value) * sum;
-      // total.value = stakePerCombination;
+function addOddsInput() {
+  let oddsCount = systemList.options[systemList.selectedIndex].innerText.slice(
+    0,
+    2
+  );
+  let systemCount = systemList.options[
+    systemList.selectedIndex
+  ].innerText.slice(
+    systemList.options[systemList.selectedIndex].innerText.length - 2
+  );
+  let placeholderIndex = 0;
+  if (parseInt(systemCount) > 6) {
+    for (let i = 0; i < parseInt(systemCount) - 6; i++) {
+      let restOddsInput = document.createElement("input");
+      restOddsInput.type = "number";
+      restOddsInput.step = "any";
+      restOddsInput.placeholder = `Odds ${placeholderIndex + (i + 7)}`;
+      restOddsInput.setAttribute("class", "odds");
+      restOddsInput.style.margin = "0rem 0.4rem 0 0";
+      let labelsCreate = document.createElement("label");
+      labelsCreate.innerText = "Correct";
+      let checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.classList.add("choice");
+      let brakes = document.createElement("br");
 
-      result.length = parseInt(
-        systemList.options[systemList.selectedIndex].innerText.charAt(0)
+      document.querySelector("#form").appendChild(restOddsInput);
+      document.querySelector("#form").appendChild(labelsCreate);
+      labelsCreate.appendChild(checkbox);
+      document.querySelector("#form").appendChild(brakes);
+    }
+  }
+}
+
+addOddsInput();
+
+// Creating banker bets input field on user select
+
+function createBankerBets() {
+  let bankerBetsCount = parseFloat(
+    bankerBets.options[bankerBets.selectedIndex].innerText
+  );
+
+  for (let i = 0; i < bankerBetsCount; i++) {
+    if (bankerBets.value != 0) {
+      bankerBetsOdds = document.createElement("input");
+      bankerBetsOdds.classList.add("banker");
+      bankerBetsOdds.placeholder = `Banker Bets ${i + 1}...`;
+      document.querySelector("#banker").appendChild(bankerBetsOdds);
+    }
+  }
+}
+createBankerBets();
+
+// below work with odds combinations and calculating final winnings
+
+calculateButton.addEventListener("click", function checkTotal() {
+  total.innerText = "";
+
+  for (i = 0; i < document.querySelectorAll(".choice").length; i++) {
+    if (document.querySelectorAll(".choice")[i].checked) {
+      console.log(document.querySelectorAll('input[type="checkbox"]:checked'));
+      let correctOdds = parseFloat(document.querySelectorAll(".odds")[i].value);
+      checkedOdds.push(correctOdds);
+      console.log(parseFloat(document.querySelectorAll(".odds")[i].value));
+
+      let bankerSum = 1;
+      document.querySelectorAll(".banker").forEach((e) => {
+        bankerSum = bankerSum * parseFloat(e.value);
+        console.log(bankerSum);
+      });
+
+      oddsCombinations.length = parseInt(
+        systemList.options[systemList.selectedIndex].innerText.slice(0, 2)
       );
-      console.log(result.length);
+
       function combine(input, len, start) {
         if (len === 0) {
-          systemCombinations.push(result.reduce((a, b) => a * b, 1));
-          systemCombinations.slice(0, result.length + 1);
-          console.log(
-            (systemCombinations[systemCombinations.length - 1] *
-              parseFloat(stake.value)) /
-              parseFloat(systemList.value)
-          );
-          winning.push(
-            (systemCombinations[systemCombinations.length - 1] *
-              parseFloat(stake.value)) /
-              parseFloat(systemList.value)
-          );
-          let totalWinning = 0;
+          systemCombinations.push(oddsCombinations.reduce((a, b) => a * b, 1));
+          systemCombinations.slice(0, oddsCombinations.length + 1);
+
+          if (bankerBets.value == 0) {
+            winning.push(
+              (systemCombinations[systemCombinations.length - 1] *
+                parseFloat(stake.value)) /
+                parseFloat(systemList.value)
+            );
+          } else {
+            winning.push(
+              systemCombinations[systemCombinations.length - 1] * bankerSum
+            );
+          }
+
+          let sum = 0;
           winning.forEach((element) => {
-            totalWinning = totalWinning + element;
-            total.value = totalWinning;
-            return totalWinning;
+            if (bankerBets.value == 0) {
+              sum = sum + element;
+            } else {
+              sum =
+                sum +
+                (element * parseFloat(stake.value)) /
+                  parseFloat(systemList.value);
+            }
+            var sue = sum.toFixed(2);
+            total.innerText = sue;
           });
 
-          console.log(result);
+          console.log(oddsCombinations);
           return;
         }
         for (let i = start; i <= input.length - len; i++) {
-          result[result.length - len] = input[i];
+          oddsCombinations[oddsCombinations.length - len] = input[i];
           combine(input, len - 1, i + 1);
         }
-        // console.log(result);
       }
-
-      // let row = 0;
-      // function multiply(a) {
-      //   row = row + a;
-      //   return row;
-      // }
     }
   }
-  combine(array, result.length, 0);
+  combine(checkedOdds, oddsCombinations.length, 0);
 });
